33a42db827979bd4a3bfd50b99534c6efe76a7b9
mhaoead112 <159633890+mhaoead112@users.noreply.github.com>
2025-08-10 19:07:00 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -33,7 +34,7 @@ require('dotenv').config();
 // menuItem.find().then((data)=> {
 //     console.log(data);
 // })
-mongoose.connect('mongodb://localhost:27017/Taiyaki-Website', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,  
   useUnifiedTopology: true
 }).then(() => {
@@ -44,10 +45,25 @@ mongoose.connect('mongodb://localhost:27017/Taiyaki-Website', {
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://taiyaki-website-cle4.vercel.app",
+  "https://taiyaki-website-cle4-git-master-mhaoead112s-projects.vercel.app",
+  "https://taiyaki-website-cle4-7htycf7z9-mhaoead112s-projects.vercel.app",
+  "*"
+];
 
 app.use(cors({
-     origin: 'http://localhost:5173' || '*'
-   }));
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.listen(PORT, ()=> {
     console.log(`Server running on ${PORT}`)    
 })
