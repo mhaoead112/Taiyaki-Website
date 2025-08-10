7b8e74824a87fecfd76d60fb44a4e3bb5eb7030f
mhaoead112 <159633890+mhaoead112@users.noreply.github.com>
2025-08-10 16:48:50 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes, BrowserRouter, Route, RouterProvider } from 'react-router-dom';
 import Navbar from './components/NavBar';
-import Menu from './pages/Menu';
-import Home from './pages/Home';
-import Cart from './pages/cart';
-import About from './pages/about';
-import Branches from './pages/branches';
-import { CartProvider } from './context/CartContext';
-import Checkout from './pages/checkout';
-import PaymentSuccess from './pages/success';
-import PaymentFailed from './pages/failure';
+import Menu from './pages/menu.jsx';
+import Home from './pages/home.jsx';
+import Cart from './pages/cart.jsx';
+import About from './pages/about.jsx';
+import Branches from './pages/branches.jsx';
+import { CartProvider } from './context/CartContext.jsx';
+import Checkout from './pages/checkout.jsx';
+import PaymentSuccess from './pages/success.jsx';
+import PaymentFailed from './pages/failure.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
